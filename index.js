48e83e8d00641aac67d687791204e757b8447e2e
Aaron <saloamj@gmail.com>
2022-12-04 21:22:05 -0600

    Added lots of functionality
    Create characters server side and send them to client side
    Able to select friendly characters and see where they can move to

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 
-// some setup help from https://socket.io/get-started/chat
+//some setup help from https://socket.io/get-started/chat
 const express = require ('express');
 const app = express();
 const http = require('http');
@@ -7,22 +7,72 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-//base get request listener
+app.use(express.static(__dirname));
+
+var users = [];
+
+//send the html file
 app.get('/', (req, res) => { 
     res.sendFile(__dirname + '/index.html');
 });
+//allows us to use local files
+app.use(express.static('public'));
+
+class Character {
+    constructor(name, movement, xPos, yPos) {
+      this.position = [xPos, yPos];
+      this.name = name;
+      this.movement = movement;
+    }
+    
+    toMsg()
+    {
+        return "|" + this.name + "," + this.movement + "," + this.position[0] + "," + this.position[1];
+    }
+  }
+
+var p1Characters = [new Character("Wilton", 3, 0, 0), new Character("golly", 2, 1, 5) ];//[NUM_ROWS-1, 0];
+
+function charsToMsg(characters)
+{
+    var msg = "";
+    for(var i = 0; i < characters.length; i++)
+    {
+        msg += characters[i].toMsg();
+    }
+    msg += ";";
+    return msg;
+}
+
+function getInitMsg(characters, num_rows, num_cols)
+{
+    return charsToMsg(characters) + num_rows + "," + num_cols;
+}
 
 io.on('connection', (socket) => {
-    console.log('a user connected');
+    var addr = socket.id;
+    users.push(addr);
+
+    var initMsg = getInitMsg(p1Characters, 5, 5);
+    console.log('User ' + users[0] + ' connected');
+    socket.emit('init',  initMsg);
+
     socket.on('disconnect', () => {
         console.log('a user disconnected');
     });
     
     socket.on('turn', (msg) => {
         console.log('user taking a turn');
-        io.emit('turn', msg);
+        var index = msg.substring(
+            msg.indexOf("{") + 1, 
+            msg.lastIndexOf("}")
+        );
+        response = "user select " + index;
+        console.log("On cell " + index);
+        io.emit('turn', response);
     });
   });  
+
 
 server.listen(3000, () => {
     console.log("Running on *:3000");
